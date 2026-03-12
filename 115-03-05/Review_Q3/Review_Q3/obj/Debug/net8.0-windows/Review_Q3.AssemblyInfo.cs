8435e083c12b40faf58b7804454845a694cfade2
a114221036-cpu <a114221036@mail.shu.edu.tw>
2026-03-12 15:56:14 +0800

    115-03-12

--- 115-03-05/Review_Q3/Review_Q3/obj/Debug/net8.0-windows/Review_Q3.AssemblyInfo.cs
+++ 115-03-05/Review_Q3/Review_Q3/obj/Debug/net8.0-windows/Review_Q3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Review_Q3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52d77b6e7180ae38cc943cac8ad33f133edcc989")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf54f0496a22b37a0c58ee0f1282ab5790d3e13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Review_Q3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Review_Q3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
